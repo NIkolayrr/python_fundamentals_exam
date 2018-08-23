f309732f5cf0357ff93b632c25d04228beffced3
NIkolayrr <niki_r95@yahoo.com>
2018-08-23 23:49:02 +0300

    Add fields to model, make migration

--- carsApi/cars/models.py
+++ carsApi/cars/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Car(models.Model):
     model = models.CharField(max_length=50)
     year = models.CharField(max_length=50)
