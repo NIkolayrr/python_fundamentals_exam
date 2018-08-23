16df3a2b772393a6d49dd0c91725894ffb309805
NIkolayrr <niki_r95@yahoo.com>
2018-08-23 23:19:33 +0300

    Initial setup

---
+++ carsApi/cars/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Car(models.Model):
+    model = models.CharField(max_length=50)
+    year = models.CharField(max_length=50)
