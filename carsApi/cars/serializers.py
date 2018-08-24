7cc40af95178c78e1a3a8fb28338b238669a40e4
NIkolayrr <niki_r95@yahoo.com>
2018-08-25 00:17:13 +0300

    Added new endpoint for models

--- carsApi/cars/serializers.py
+++ carsApi/cars/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Car
+from .models import Car, CarModels
 
 
 class CarSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ('url', 'id', 'model', 'year', 'price')
+
+
+class CarModelSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = CarModels
+        fields = ('url', 'id', 'name')
--- carsApi/cars/views.py
+++ carsApi/cars/views.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from rest_framework import viewsets
-from .models import Car
-from .serializers import CarSerialiser
+from .models import Car, CarModels
+from .serializers import CarSerialiser, CarModelSerialiser
 
 
 class CarList(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerialiser
     http_method_names = ['get', 'post']
+
+
+class CarModelView(viewsets.ModelViewSet):
+    queryset = CarModels.objects.all()
+    serializer_class = CarModelSerialiser
